d735ef4d2769ffd74145fceaebbb61c70fe07689
T-107 <T-107@PC258>
2015-10-01 18:48:34 -0500

    lunes

--- lemguajes-140915/src/lemguajes/pkg140915/Imc.java
+++ lemguajes-140915/src/lemguajes/pkg140915/Imc.java
@@ -11,18 +11,61 @@ package lemguajes.pkg140915;
  */
 public class Imc {
     
-   float peso;
-   float altura;
-   
-   public Imc(float p, float a){   //inicializar IMPLEMENTACION 
-       peso=a;
-       altura=p;
-      }
-     
-            
-public float calcular(){
-    float resultadoImc=peso/(altura*altura);
-    return resultadoImc;
-} 
+    
+    float peso; //Mi primer variable
+    float altura; // Mi segundo variable
+    
+    public  Imc (float p, float a){ // Varibles locales.  Constructor
         
+        peso = p;
+        altura = a;
+    }
+    
+    public Imc () { //Inicializar los atributos.
+        
+     peso = 78;
+     altura = (float) 1.90;
+        
+    }
+    
+    public Imc (float f) {
+        //ESTE CODIGO NO TYIENE NADA QUE VER CON LOS CONSTRUTORES
+        byte b=2;
+        short s=2;
+        int i=2;
+        long l=2;
+        //Reasignamos tipos no a los valores
+        //Casting (promocion)
+        b=(byte)i;
+        l=i;
+        s=(short)l;
+        s=b;
+        l=s;
+        s=(short)l;
+        s=(short)i;
+        l=b;
+        
+        
+        
+    }
+    
+    //Mi tecer variable
+    public String calcular () { //Mi tipo de valor que me volvera, no lo ve el main.
+        
+        String mensaje ="nada";
+        
+      float resultadoImc = peso/(altura*altura);
+        // Me regresa mi resultadoImc
+     if ( resultadoImc < 20)mensaje ="eres anorexica" ;
+           
+    else if(resultadoImc>=20 || resultadoImc <25) mensaje="estas bien";
+       
+    else if(resultadoImc >=25 || resultadoImc <30)mensaje="te pasaste";
+    
+     else mensaje ="eres un obeso";
+          
+   return mensaje + "tu imc es:" + resultadoImc;
+ 
+    }
 }
+
