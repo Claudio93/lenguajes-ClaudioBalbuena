43d29993d41ee12cf91fa234168230d54b6e8eb2
T-107 <T-107@PC256>
2015-10-08 18:44:51 -0500

    08/10/2015

---
+++ lemguajes-140915/src/Archivaldo/InterfazDeUsuario.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Archivaldo;
+
+/**
+ *
+ * @author T-107
+ */
+public class InterfazDeUsuario {
+    
+    public static void main(String[] args) {
+        
+        
+        
+    }
+}
