a6609b43a16e0267f0e34394fe4d785a87d5c3b9
T-107 <T-107@PC258>
2015-10-26 18:43:06 -0600

    examen

---
+++ lemguajes-140915/src/EjertipoExamen/carrito.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package EjertipoExamen;
+
+/**
+ *
+ * @author T-107
+ */
+public class carrito {
+    
+   /**
+   Se desea desarollar un sistema de compras con un carrito electronico 
+   en el este es una clase**carritoelectronico** se pueden agregar **productos**  de un **cliente** en particular, 
+   de los productos 
+   se llene  su nombre, costo y departamento (lacteos,cereales,carnes,etc) y del cliente el tipo de
+   pago (tarjeta efectivo vales) y su mail.
+   * 
+   * 1- desarrollar el diagrama uml del sistema con las clases que considere necesarias 
+   * 2- codificar las clases encpsulamiento 
+   * 3- crear una clase q se llame cuentacliente que guarda en un archivo 
+   * serealizado un arraylist generico a la clase carritoElectronico codificar el 
+   * metodo que regree todos los objetos de tipo  carritolectronico que se han guardado 
+   * --atributos = productos clientes costos depatamentos tipo pago email
+   */
+    
+    
+}
