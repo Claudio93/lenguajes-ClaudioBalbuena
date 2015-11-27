8a9bde2c093f6f8ad22b190ad634516990636618
T-107 <T-107@PC256>
2015-11-26 18:40:29 -0600

    hoy 26/11/2015

---
+++ lemguajes-140915/src/previewjava3/Raton.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package previewjava3;
+
+/**
+ *
+ * @author T-107
+ */
+public class Raton {
+       public String hacerRuido(){
+        return "iiiii-iiii-iiii";
+    }
+    
+}
