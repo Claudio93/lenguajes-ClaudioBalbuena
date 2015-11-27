ae06fd63f2c2b87c2bbe2039b9e7bb7893a403a0
T-107 <T-107@PC256>
2015-11-26 18:54:14 -0600

    hoy 26/11/2015-

---
+++ lemguajes-140915/src/previewjava3/AplicacionAnimalitos.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package previewjava3;
+
+/**
+ *
+ * @author T-107
+ */
+public class AplicacionAnimalitos {
+    public static void main(String[] args) {
+        Raton r = new Raton();
+        
+        System.out.println(botonSonido(r));
+        //Aqui el niño seleccionara el listado de un animalito
+        //por ejemplo el raton
+        
+        
+        
+    }
+    
+    public static String botonSonido(Raton r){
+        return r.hacerRuido();
+        }
+
+
+}
